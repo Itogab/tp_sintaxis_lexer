3f82f4083dbe9b884f498f62ada00c7b8cb32537
ToGabI <togithurburu@gmail.com>
2024-05-31 14:59:29 -0300

    Añadi el automata coma punto

---
+++ Automatas/Coma_punto.py
@@ -0,0 +1,18 @@
+def automata_punto_coma(lexema):
+    estado=0
+    estados_finales = [1]
+    delta= {0:{'.':1,',':1},1:{}}
+    
+    for caracter in lexema:
+        if caracter in delta[estado].keys():
+            estado=delta[estado][caracter]
+        else:
+            estado = -1
+            break
+    if estado == -1:
+        return ESTADO_TRAMPA
+    if estado in estados_finales:
+        return ESTADO_FINAL
+    else:
+        return ESTADO_NO_FINAL
+    

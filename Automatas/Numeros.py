82d47a554d7f1a5e05507e083c5583c195f0080f
Itogab <99827676+Itogab@users.noreply.github.com>
2024-05-31 14:45:21 -0300

    Add files via upload
    Añadi los automatas 'call', 'id', 'numeros'

---
+++ Automatas/Numeros.py
@@ -0,0 +1,18 @@
+def automata_num(lexema):
+    estado = 0
+    estados_finales = [1]
+    delta= {0:{'0':1,'1':1,'2':1,'3':1,'4':1,'5':1,'6':1,'7':1,'8':1,'9':1}
+            ,1:{'0':1,'1':1,'2':1,'3':1,'4':1,'5':1,'6':1,'7':1,'8':1,'9':1}}
+    
+    for caracter in lexema:
+        if caracter in delta[estado].keys():
+            estado=delta[estado][caracter]
+        else:
+            estado = -1
+            break
+    if estado == -1:
+        return ESTADO_TRAMPA
+    if estado in estados_finales:
+        return ESTADO_FINAL
+    else:
+        return ESTADO_NO_FINAL

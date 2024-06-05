6602dd94c5432321e040e4430cbec7221acd9405
ToGabI <togithurburu@gmail.com>
2024-06-05 14:54:50 -0300

    Mejorado funcionalidad lexer y agregado automata OperadorAsing

--- main.py
+++ main.py
@@ -15,14 +15,15 @@ from Automatas.Puntos import *
 
 ESTADO_FINAL = "ESTADO FINAL"
 ESTADO_NO_FINAL = "NO ACEPTADO"
-ESTADO_TRAMPA = "EN ESTADO TRAMPA" # La distinción entre estado trampa y no final es porque si bien en ambos casos la cadena 
-                                   # actual no es aceptada, estando en un estado final ante el siguiente carácter de entrada el 
-                                   # estado puede cambiar, mientras en el trampa no pues es un pozo
-TOKENS_POSIBLES = [('TOKENID',automata_id)]
+ESTADO_TRAMPA = "EN ESTADO TRAMPA" 
+TOKENS_POSIBLES = [('TOKENNUM',automata_num),('TOKENCALL',automata_call),('TOKENPUNTOCOMA',automata_punto_coma),('TOKENASIGN',automata_asign),('TOKENID',automata_id)]
+
+
 def lexer(codigo_fuente):
     tokens = [] # listada de tokens que devolverá el lexer correspondiente al código fuente ingresado, se inicializa vacia
     posicion_actual = 0   # carácter actual en el código fuente que se esta procesando
     while posicion_actual < len(codigo_fuente):
+
         comienzo_lexema = posicion_actual # el siguiente lexema comienza en la última posición procesada
         posibles_tokens = [] # categorías de tokens posibles para el lexema actual
         posibles_tokens_con_un_caracter_mas = [] # categorías de tokens posibles para el lexema actual mas el próximo caracter
@@ -31,23 +32,32 @@ def lexer(codigo_fuente):
 
         while not var_aux_todos_en_estado_trampa:
             var_aux_todos_en_estado_trampa = True
-            lexema = codigo_fuente[comienzo_lexema:posicion_actual + 1]
-            posibles_tokens = posibles_tokens_con_un_caracter_mas
+            aux = lexema
+            lexema = codigo_fuente[comienzo_lexema:(posicion_actual + 1)]
             posibles_tokens_con_un_caracter_mas = []
-
-            for (un_tipo_de_token, afd) in TOKENS_POSIBLES:
-                simulacion_afd = afd(lexema) # simula la ejecución de cada AFD para el lexema actual agregando un carácter
-                if simulacion_afd == ESTADO_FINAL:
-                    posibles_tokens_con_un_caracter_mas.append(un_tipo_de_token)
-                    var_aux_todos_en_estado_trampa = False
-                elif simulacion_afd == ESTADO_NO_FINAL:
-                    var_aux_todos_en_estado_trampa = False
             
+            if aux != lexema:
+                for (un_tipo_de_token, afd) in TOKENS_POSIBLES:
+                    simulacion_afd = afd(lexema) # simula la ejecución de cada AFD para el lexema actual agregando un carácter
+                    if simulacion_afd == ESTADO_FINAL:
+                        if un_tipo_de_token != 'TOKENESPACIO':
+                            posibles_tokens_con_un_caracter_mas.append(un_tipo_de_token)
+                            var_aux_todos_en_estado_trampa = False
+                        else:
+                            var_aux_todos_en_estado_trampa = True
+                    elif simulacion_afd == ESTADO_NO_FINAL:
+                        var_aux_todos_en_estado_trampa = False
+            else:
+                var_aux_todos_en_estado_trampa = True
+                break
+            if len(posibles_tokens_con_un_caracter_mas) != 0:
+                posibles_tokens = posibles_tokens_con_un_caracter_mas
+                
             posicion_actual = posicion_actual + 1 #Convierte la posición simulada en la actual, ya sea para continuar avanzando en 
                                                   # en el lexema actual, o convertir en la posición inicial del próximo
 
         if len(posibles_tokens) == 0:
-            raise Exception("ERROR:TOKEN DESCONOCIDO" + lexema)
+            raise Exception("ERROR:TOKEN DESCONOCIDO " + lexema)
 
         un_tipo_de_token = posibles_tokens[0] # Mientras haya posibilidad de avanzar, ya sea por estar en un estado final o no 
                                               # final el algoritmo avanza, con lo cual siempre devuelve el lexema más largo que 
@@ -55,7 +65,6 @@ def lexer(codigo_fuente):
                                               # según la precedencia elegida por el creador del lexer en TOKENS_POSIBLES
         token = (un_tipo_de_token, lexema)
         tokens.append(token)
-
     return tokens
 
-print(lexer("cadena de prueba"))
+print(lexer('var nombre := hola'))

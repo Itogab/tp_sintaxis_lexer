4b77a5034ee3dfcbeed5306f775980519be85b65
gustipardo <gusm200@gmail.com>
2024-06-12 09:52:33 -0300

    Operador Relacional, ultimos dos prints no tira estado trampa

--- Automatas/OperadorRel.py
+++ Automatas/OperadorRel.py
@@ -1,3 +1,34 @@
 ESTADO_FINAL = "ESTADO FINAL"
 ESTADO_NO_FINAL = "NO ACEPTADO"
 ESTADO_TRAMPA = "EN ESTADO TRAMPA"
+
+def automata_relop(lexema):
+    estado = 0
+    estados_finales = [1, 2]
+    delta = {
+        0: {'<': 1, '>': 1, '=': 1, '!': 2},
+        1: {'=': 2},
+        2: {'=': 1}
+    }
+    
+    for caracter in lexema:
+        if estado in delta and caracter in delta[estado]:
+            estado = delta[estado][caracter]
+        else:
+            estado = -1
+            break
+    
+    if estado == -1:
+        return ESTADO_TRAMPA
+    if estado in estados_finales:
+        return ESTADO_FINAL
+    else:
+        return ESTADO_NO_FINAL
+
+print(automata_relop('<'))
+print(automata_relop('<='))
+print(automata_relop('!='))
+
+print(automata_relop('<<')) 
+print(automata_relop('<=='))
+print(automata_relop('!=='))

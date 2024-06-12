231721166d0ef01aa3f960cd50e5eb616af5d88c
gustipardo <gusm200@gmail.com>
2024-06-11 21:24:38 -0300

    Automata Numeral

--- Automatas/Numeral.py
+++ Automatas/Numeral.py
@@ -1,3 +1,27 @@
 ESTADO_FINAL = "ESTADO FINAL"
 ESTADO_NO_FINAL = "NO ACEPTADO"
 ESTADO_TRAMPA = "EN ESTADO TRAMPA"
+
+def automata_numeral(lexema):
+    estado = 0
+    estados_finales = [1]
+    delta = {
+        0: {'#': 1}
+    }
+    
+    for caracter in lexema:
+        if estado in delta and caracter in delta[estado]:
+            estado = delta[estado][caracter]
+        else:
+            estado = -1
+            break
+    
+    if estado == -1:
+        return ESTADO_TRAMPA
+    if estado in estados_finales:
+        return ESTADO_FINAL
+    else:
+        return ESTADO_NO_FINAL
+
+print(automata_numeral('#'))
+print(automata_numeral('##'))

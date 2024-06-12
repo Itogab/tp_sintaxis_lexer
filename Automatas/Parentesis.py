c151c77dbcdcf311ac6617d711e73298978e0780
gustipardo <gusm200@gmail.com>
2024-06-12 11:04:29 -0300

    Automata de parentesis

--- Automatas/Parentesis.py
+++ Automatas/Parentesis.py
@@ -1,3 +1,30 @@
 ESTADO_FINAL = "ESTADO FINAL"
 ESTADO_NO_FINAL = "NO ACEPTADO"
 ESTADO_TRAMPA = "EN ESTADO TRAMPA"
+
+def automata_parentesis(lexema):
+    estado = 0
+    estados_finales = [1]
+    delta = {
+        0: {'(': 1, ')': 1}
+    }
+    
+    for caracter in lexema:
+        if estado in delta and caracter in delta[estado]:
+            estado = delta[estado][caracter]
+        else:
+            estado = -1
+            break
+    
+    if estado == -1:
+        return ESTADO_TRAMPA
+    if estado in estados_finales:
+        return ESTADO_FINAL
+    else:
+        return ESTADO_NO_FINAL
+
+print(automata_parentesis('('))
+print(automata_parentesis(')'))
+
+print(automata_parentesis('))'))
+print(automata_parentesis(')()'))

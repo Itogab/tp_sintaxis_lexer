89aba5e6792957d90ef3024e4c33cf70808c9df1
ToGabI <togithurburu@gmail.com>
2024-10-14 17:00:23 -0300

    Añadidas producciones

--- parser.py
+++ parser.py
@@ -1,10 +1,30 @@
 from lex import *
 
-VN = ['S']
+VN = ['Program','Block','ConstDecl','ConstAssigList','CAS','VarDecl','IdList','IL','ProcDecl','PD','Statement','StatementList','SL','Condition','Relation','Expression','E','SumOperator','Term','T','MultOperator','Factor']
 
-VT = ['Token(','Token)']
+VT = ['TOKENPROCEDURE','TOKENPARENTESISA','TOKENPARENTESISC','TOKENNUM','TOKENVAR','TOKENCONST','TOKENCALL','TOKENBEGIN','TOKENEND','TOKENIF','TOKENODD','TOKENTHEN','TOKENWHILE','TOKENPUNTOYCOMA','TOKENCOMA','TOKENPUNTO','TOKENESPACIO','TOKENSUMARESTA','TOKENMULTDIV','TOKENASIGN','TOKENPUNTO','TOKENOPERADORREL','TOKENID']
 
-P = {
+P = {'Program':[['Block','TOKENEOF']],
+     'Block':[['ConstDecl','VarDecl','ProcDecl','Statement'],['ConstDecl'],['VarDecl'],['ProcDecl'],['Statement'],['ConstDecl','VarDecl'],['ConstDecl','ProcDecl'],['ConstDecl','Statement'],['VarDecl','ProcDecl'],['VarDecl','Statement'],['ProcDecl','Statement'],['ConstDecl','VarDecl','ProcDecl'],['ConstDecl','VarDecl','Statement'],['VarDecl','ProcDecl','Statement'],['ConstDecl','ProcDecl','Statement']],
+     'ConstDecl':[['TOKENCONST','ConstAssigList','TOKENPUNTOYCOMA']],
+     'CAS':[['TOKENCOMA','TOKENID','TOKENOPERADORREL','CAS'],['TOKENCOMA','TOKENID','TOKENOPERADORREL']],
+     'VarDecl':[['TOKENVAR','IdList']],
+     'IdList':[['TOKENID','IL'],['TOKENID']],
+     'IL':[['TOKENCOMA','TOKENID','IL'],['TOKENCOMA','TOKENID']],
+     'ProcDecl':[['PD']],
+     'PD':[['TOKENPROCEDURE','TOKENID','TOKENPUNTOYCOMA','Block','TOKENPUNTOYCOMA','PD'],['TOKENPROCEDURE','TOKENID','TOKENPUNTOYCOMA','TOKENPUNTOYCOMA','PD'],['TOKENPROCEDURE','TOKENID','TOKENPUNTOYCOMA','Block','TOKENPUNTOYCOMA'],['TOKENPROCEDURE','TOKENID','TOKENPUNTOYCOMA','TOKENPUNTOYCOMA']],
+     'Statement':[['TOKENID','TOKENASIGN','Expression'],['TOKENCALL','TOKENID'],['TOKENBEGIN','StatementList','TOKENEND'],['TOKENBEGIN','TOKENEND'],['TOKENIF','Condition','TOKENTHEN','Statement'],['TOKENIF','Condition','TOKENTHEN'],['TOKENWHILE','Condition','TOKENDO','Statement'],['TOKENWHILE','Condition','TOKENDO']],
+     'StatementList':[['Statement','SL'],['Statement'],['SL']],
+     'SL':[['TOKENPUNTOYCOMA','Statement','SL'],['TOKENPUNTOYCOMA','Statement'],['TOKENPUNTOYCOMA','SL']],
+     'Condition':[['Expression','Relation','Expression'],['TOKENODD','Expression']],
+     'Relation':[['TOKENOPERADORREL']],
+     'Expression':[['SumOperator','Term','E'],['SumOperator','Term'],['Term','E'],['Term']],
+     'E':[['SumOperator','Term','E'],['SumOperator','Term']],
+     'SumOperator':[['TOKENSUMARESTA']],
+     'Term':[['Factor','T'],['Factor']],
+     'T':[['MultOperator','Factor','T'],['MultOperator','Factor']],
+     'MultOperator':[['TOKENMULTDIV']],
+     'Factor':[['TOKENPARENTESISA','Expression','TOKENPARENTESISC'],['TOKENID'],['TOKENNUM']],
     'S':[['Token(','S','Token)','S'],['Token(','Token)','S'],['Token(','S','Token)'],['Token(','Token)']]
 }
 
